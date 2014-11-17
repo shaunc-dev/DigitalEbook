db2145aae2f98fb075cc6360f2b158b07d1da073
Shaun Calagos <scalagos0123@live.com>
2014-11-17 12:00:51 +0800

    Ebook

---
+++ myjs.js
@@ -0,0 +1,36 @@
+function bprofile() {
+
+	$(document).ready(function() {
+		$('#bbooks').hide();
+		$('#bupload').hide();
+		$('#bactivities').hide();
+		$('#bprofile').show();
+	});
+}
+
+function bbooks() {
+	$(document).ready(function() {
+		$('#bbooks').show();
+		$('#bupload').hide();
+		$('#bactivities').hide();
+		$('#bprofile').hide();
+	});
+}
+
+function bupload() {
+	$(document).ready(function() {
+		$('#bbooks').hide();
+		$('#bupload').show();
+		$('#bactivities').hide();
+		$('#bprofile').hide();
+	});
+}
+
+function bupload() {
+	$(document).ready(function() {
+		$('#bbooks').hide();
+		$('#bupload').hide();
+		$('#bactivities').show();
+		$('#bprofile').hide();
+	});
+}
